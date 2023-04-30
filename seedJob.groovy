39778488272a832f042a21c2ce62067a2747c2cc
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-04-30 10:09:56 +0530

    Update seedJob.groovy

--- seedJob.groovy
+++ seedJob.groovy
@@ -1,47 +1,39 @@
-pipelineJob('MyParameterizedPipeline') {
-  definition {
-    cps {
-      script("""
-        pipeline {
-          agent any
-          options {
-            parameters {
-              string(name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment')
-              booleanParam(name: 'CLEAN_BUILD', defaultValue: false, description: 'Perform clean build?')
-            }
-          }
-          stages {
-            stage('Build') {
-              steps {
-                echo 'Building...'
-                sh 'mvn clean install' // Example build step
-              }
-            }
-            stage('Test') {
-              steps {
-                echo 'Testing...'
-                sh 'mvn test' // Example test step
-              }
-            }
-            stage('Deploy') {
-              steps {
-                echo "Deploying to \${params.ENVIRONMENT} environment..."
-                sh '''
-                  if [ "\${params.ENVIRONMENT}" == "dev" ]; then
-                    echo "Deploying to Dev environment..."
-                    # Add your deployment commands here for the dev environment
-                  elif [ "\${params.ENVIRONMENT}" == "prod" ]; then
-                    echo "Deploying to Prod environment..."
-                    # Add your deployment commands here for the prod environment
-                  else
-                    echo "Invalid environment specified!"
-                  fi
-                '''
-              }
-            }
-          }
-        }
-      """)
+pipeline {
+  agent any
+
+  parameters {
+    string(name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Environment')
+    booleanParam(name: 'CLEAN_BUILD', defaultValue: false, description: 'Perform clean build?')
+  }
+
+  stages {
+    stage('Build') {
+      steps {
+        echo 'Building...'
+        sh 'mvn clean install' // Example build step
+      }
+    }
+    stage('Test') {
+      steps {
+        echo 'Testing...'
+        sh 'mvn test' // Example test step
+      }
+    }
+    stage('Deploy') {
+      steps {
+        echo "Deploying to \${params.ENVIRONMENT} environment..."
+        sh '''
+          if [ "\${params.ENVIRONMENT}" == "dev" ]; then
+            echo "Deploying to Dev environment..."
+            # Add your deployment commands here for the dev environment
+          elif [ "\${params.ENVIRONMENT}" == "prod" ]; then
+            echo "Deploying to Prod environment..."
+            # Add your deployment commands here for the prod environment
+          else
+            echo "Invalid environment specified!"
+          fi
+        '''
+      }
     }
   }
 }
