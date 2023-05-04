5d94ae6a84f47e2e01c994633144b17088284987
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-05-04 11:25:47 +0530

    Deleting

--- Terraform/ec2.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "ami_details" {
-  most_recent = true
-  owners      = ["amazon"]
-}
-
-resource "aws_instance" "Test_EC2" {
-  ami           = data.aws_ami.ami_details
-  instance_type = "t2.micro"
-}
