37c38dff69cce7aa86d08772b6e1a0b8c2616756
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-05-04 11:29:50 +0530

    Updated the pipeline job

--- deploy_ec2.groovy
+++ deploy_ec2.groovy
@@ -1,42 +1,52 @@
-pipeline {
-  agent any
-
-  environment {
-    TF_VAR_environment = env.ENVIRONMENT
-    TF_VAR_gitbranch = env.GIT_BRANCH
-    AWS_ACCESS_KEY_ID = credentials('aws-access-key-id')
-    AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
-  }
-
-  stages {
-    stage('Checkout') {
-      steps {
-        git branch: env.GIT_BRANCH, url: 'https://github.com/kunal8817/DBscripts.git'
-      }
-    }
-
-    stage('Terraform Init') {
-      steps {
-        dir('Terraform') {
-          sh 'terraform init'
+pipelineJob('Deploy_job') {
+  definition {
+    cps {
+      script("""
+        pipeline {
+          agent any
+    
+          environment {
+            TF_VAR_environment = params.environment
+            TF_VAR_gitbranch = params.gitbranch
+    
+            AWS_ACCESS_KEY_ID = credentials('aws-access-key-id')
+            AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
+          }
+    
+          stages {
+            stage('Checkout') {
+              steps {
+                git branch: params.gitbranch, url: 'https://github.com/kunal8817/DBscripts.git'
+              }
+            }
+    
+            stage('Terraform Init') {
+              steps {
+                dir('terraform') {
+                  sh 'terraform init'
+                }
+              }
+            }
+    
+            stage('Terraform Plan') {
+              steps {
+                dir('terraform') {
+                  sh 'terraform plan -var environment=${params.environment} -var gitbranch=${params.gitbranch}'
+                }
+              }
+            }
+    
+            stage('Terraform Apply') {
+              steps {
+                dir('terraform') {
+                  sh 'terraform apply -auto-approve -var environment=${params.environment} -var gitbranch=${params.gitbranch}'
+                }
+              }
+            }
+          }
         }
-      }
-    }
-
-    stage('Terraform Plan') {
-      steps {
-        dir('Terraform') {
-          sh "terraform plan -var environment=${env.ENVIRONMENT} -var gitbranch=${env.GIT_BRANCH}"
-        }
-      }
-    }
-
-    stage('Terraform Apply') {
-      steps {
-        dir('terraform') {
-          sh "terraform apply -auto-approve -var environment=${env.ENVIRONMENT} -var gitbranch=${env.GIT_BRANCH}"
-        }
-      }
+      """)
+      sandbox()
     }
   }
 }
