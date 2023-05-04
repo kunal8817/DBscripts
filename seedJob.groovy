a30309d77ec573aa64ae1f9296279c4921ffc2ee
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-05-04 12:55:51 +0530

    Again updated the choiceParam

--- seedJob.groovy
+++ seedJob.groovy
@@ -1,8 +1,8 @@
 job('SEED_JOB_TERRAFORM_DEPLOY') {
   properties {
     parameters {
-      choice(name: 'ENVIRONMENT', choices: ['dev', 'prod'], description: 'Select the Environment.')
-      choice(name: 'GIT_BRANCH', '', choices: ['main', 'feature/terraform'], description: 'Select the branch')
+      choiceParam(name: 'ENVIRONMENT', choices: ['dev', 'prod'], description: 'Select the Environment.')
+      choiceParam(name: 'GIT_BRANCH', '', choices: ['main', 'feature/terraform'], description: 'Select the branch')
       credentialsParam('AWS_ACCESS_KEY_ID', '', 'AWS Access Key ID', 'aws-access-key-id')
       credentialsParam('AWS_SECRET_ACCESS_KEY', '', 'AWS Secret Access Key', 'aws-secret-access-key')
     }
