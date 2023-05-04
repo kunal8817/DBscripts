f4f47434a05e8c82d3e7109cea24634bc416e44f
kunal <kunal8817@github.com>
2023-05-04 11:13:41 +0530

    Updating the terraform code

--- seedJob.groovy
+++ seedJob.groovy
@@ -1,45 +1,15 @@
-pipelineJob('MyParameterizedPipeline') {
-  definition {
-    cps {
-      script("""
-        pipeline {
-          agent any
-          parameters {
-            choice(name: 'ENVIRONMENT', choices: 'dev', description: 'Environment')
-            booleanParam(name: 'CLEAN_BUILD', defaultValue: false, description: 'Perform clean build?')
-          }
-          stages {
-            stage('Listing_version') {
-              steps {
-                echo 'Loading...'
-                sh 'terraform --version' // Listing the terraform version
-              }
-            }
-            stage('Test') {
-              steps {
-                echo 'Testing...'
-                sh 'terraform --version' // Example test step
-              }
-            }
-            stage('Deploy') {
-              steps {
-                echo "Deploying to \${params.ENVIRONMENT} environment..."
-                sh '''
-                  if [ "\${params.ENVIRONMENT}" == "dev" ]; then
-                    echo "Deploying to Dev environment..."
-                    # Add your deployment commands here for the dev environment
-                  elif [ "\${params.ENVIRONMENT}" == "prod" ]; then
-                    echo "Deploying to Prod environment..."
-                    # Add your deployment commands here for the prod environment
-                  else
-                    echo "Invalid environment specified!"
-                  fi
-                '''
-              }
-            }
-          }
-        }
-      """)
+job('SEED_JOB_TERRAFORM_DEPLOY') {
+  properties {
+    parameters {
+      stringParam('ENVIRONMENT', '', 'Environment (dev, prod)')
+      stringParam('GIT_BRANCH', '', 'Git branch (main, feature/terraform)')
+      credentialsParam('AWS_ACCESS_KEY_ID', '', 'AWS Access Key ID', 'aws-access-key-id')
+      credentialsParam('AWS_SECRET_ACCESS_KEY', '', 'AWS Secret Access Key', 'aws-secret-access-key')
+    }
+  }
+  steps {
+    dsl {
+      external('deploy_ec2.groovy')
     }
   }
 }
