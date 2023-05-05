46bf5d189c28675cb56553088e611c4cd2fa574a
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-05-05 11:22:35 +0530

    Update seedJob.groovy

--- seedJob.groovy
+++ seedJob.groovy
@@ -1,8 +1,6 @@
 job('Seed Job') {
   properties {
     parameters {
-      stringParam('ENVIRONMENT', '', 'Environment (dev, prod)')
-      stringParam('GIT_BRANCH', '', 'Git branch (main, feature)')
       credentialsParam('AWS_ACCESS_KEY_ID', '', 'AWS Access Key ID', 'aws-access-key-id')
       credentialsParam('AWS_SECRET_ACCESS_KEY', '', 'AWS Secret Access Key', 'aws-secret-access-key')
     }
