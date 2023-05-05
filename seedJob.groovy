78b7d80e9041587aa9043b098f2f2f19b6ff1b84
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-05-05 11:24:17 +0530

    Update seedJob.groovy

--- seedJob.groovy
+++ seedJob.groovy
@@ -1,10 +1,10 @@
 job('Seed Job') {
-  properties {
+  /*properties {
     parameters {
       credentialsParam('AWS_ACCESS_KEY_ID', '', 'AWS Access Key ID', 'aws-access-key-id')
       credentialsParam('AWS_SECRET_ACCESS_KEY', '', 'AWS Secret Access Key', 'aws-secret-access-key')
     }
-  }
+  }*/
   steps {
     dsl {
       external('deploy_ec2.groovy')
