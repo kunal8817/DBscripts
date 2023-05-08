90cfd2b79d339bcd6ad9b753905603af77257e57
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-05-08 15:29:26 +0530

    Update deploy_ec2.groovy

--- deploy_ec2.groovy
+++ deploy_ec2.groovy
@@ -2,8 +2,6 @@ pipelineJob('Deploy_job') {
     parameters {
       stringParam('ENVIRONMENT', 'Environment (dev, prod)')
       stringParam('GIT_BRANCH', 'Git branch (main, feature/terraform)')
-      credentialsParam('AWS_ACCESS_KEY_ID', 'AWS Access Key ID', 'aws-access-key-id')
-      credentialsParam('AWS_SECRET_ACCESS_KEY', 'AWS Secret Access Key', 'aws-secret-access-key')
   }
   definition {
     cps {
