685f56a0450c183ef284370739886afea1c1d50a
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-05-21 17:45:00 +0530

    Update workspace.groovy

--- workspace.groovy
+++ workspace.groovy
@@ -11,8 +11,6 @@ pipelineJob('Terraform_Create_Workspace') {
   definition {
     cps {
       script(readFileFromWorkspace('Terraform_workspace.groovy'))
-      sandbox()
-      args(params)
     }
   }
 }
