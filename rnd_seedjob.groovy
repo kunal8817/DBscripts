35aa13cddfe74493bcde05fe7eac342a9f89920d
kunal <kunal8817@github.com>
2023-05-22 12:12:40 +0530

    Updated

--- rnd_seedjob.groovy
+++ rnd_seedjob.groovy
@@ -4,7 +4,7 @@ def jobDslScript = "test_pipelines_rnd_seedjob.groovy"
 pipelineJob('rnd-seedjob-test') {
     description('Pipeline job for new rnd work')
     parameters{
-        choice choices: ['https://github.com/kunal8817/DBscripts.git'], description: 'Jenkins Git URL', name: 'gitUrl'
+        choice choices: ['${gitUrl}'], description: 'Jenkins Git URL', name: 'gitUrl'
         choice choices: ['main', 'feature'], name: 'gitBranchType'
         choice choices: ['main'], description: 'Branch Name', name: 'gitBranchName'
     }
