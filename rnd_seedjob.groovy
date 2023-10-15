bee387e5558d32d7e433c83f41203f6b467bac31
kunal8817 <60497098+kunal8817@users.noreply.github.com>
2023-10-15 10:17:14 +0530

    Update rnd_seedjob.groovy

--- rnd_seedjob.groovy
+++ rnd_seedjob.groovy
@@ -2,10 +2,6 @@ def jobDslScript = "test_pipelines_rnd_seedjob.groovy"
 
 pipelineJob('rnd-seedjob-test') {
     description('Pipeline job for new rnd work')
-    parameters{
-        choice choices: ['main', 'feature'], name: 'gitBranchType'
-        choice choices: ['main'], description: 'Branch Name', name: 'gitBranchName'
-    }
 
     definition {
         cps {
